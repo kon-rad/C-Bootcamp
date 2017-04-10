5030cc1a72ad4f7d5f984afc44dfbdd42a89c7d8
konradgnat <konradmgnat@gmail.com>
2017-04-09 17:11:30 -0700

    Add rush00

---
+++ rush00/rush02.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgnat <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/09 15:40:15 by kgnat             #+#    #+#             */
+/*   Updated: 2017/04/09 16:20:51 by kgnat            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_putchar(char c);
+
+void	print_char(int x_i, int y_i, int x, int y)
+{
+	if ((y_i == 1) && ((x_i == 1) || (x_i == x)))
+		ft_putchar('A');
+	else if ((y_i == y) && ((x_i == 1) || (x_i == x)))
+		ft_putchar('C');
+	else if (((y_i == 1) || (y_i == y)) && ((x_i != 1) || (x_i != x)))
+		ft_putchar('B');
+	else if (((y_i != 1) || (y_i != y)) && ((x_i == 1) || (x_i == x)))
+		ft_putchar('B');
+	else if (((x_i != 1) || (x_i != x)) && ((y_i != 1) || (y_i != y)))
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int x_i;
+	int y_i;
+
+	if (x > 0 && y > 0)
+	{
+		y_i = 1;
+		while (y_i <= y)
+		{
+			x_i = 1;
+			while (x_i <= x)
+			{
+				print_char(x_i, y_i, x, y);
+				x_i++;
+			}
+			ft_putchar('\n');
+			y_i++;
+		}
+	}
+}
